8f9993b4c5f6fa737cdcdf7397ec7145015df732
Mac <mac@Macs-MacBook-Pro.local>
2018-08-06 07:34:43 +0100

    add flex layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css';
+import UserScreen from './screens/UserScreen'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<UserScreen />, document.getElementById('root'));
