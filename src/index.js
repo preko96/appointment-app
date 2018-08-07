4b30374324eb34b33ff568b73147f8e670bd591f
Mac <mac@Macs-MacBook-Pro.local>
2018-08-07 23:55:02 +0100

    delete unneccesary init files.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'antd/dist/antd.css'
 import UserScreen from './screens/UserScreen'
 
-ReactDOM.render(<UserScreen />, document.getElementById('root'));
+ReactDOM.render(<UserScreen />, document.getElementById('root'))
